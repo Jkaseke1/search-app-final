ac4ee2feab1d4dd93f886d702b48385c1f13036a
Jkaseke1 <166221711+Jkaseke1@users.noreply.github.com>
2024-08-02 16:59:23 +0200

    Add files via upload

---
+++ config/origins.module.js
@@ -0,0 +1,11 @@
+const origins = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'http://localhost:3002',
+  'http://localhost:3003',
+  'http://localhost:8000',
+  'http://localhost:8080',
+  'http://localhost:5000',
+];
+
+module.exports = { origins };
